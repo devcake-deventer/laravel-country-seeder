6d6933a271e57b1527cbb6217c33ca029ff21e6a
Mike Slinkman <m.slinkman@devcake.nl>
2020-09-11 14:14:33 +0200

    Added support for seeding nationalities to a configurable table

---
+++ database/migrations/create_nationalities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNationalitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->getTableName(), function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code');
+            $table->longText('nationality');
+            $table->timestamps();
+        });
+
+    }
+
+    public function getTableName()
+    {
+        return config('CountrySeeder.nationalities_table_name');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->getTableName());
+    }
+}
