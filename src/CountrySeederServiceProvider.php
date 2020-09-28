0aeb0975a7e45f4c822a36eebf173d671e2f327f
Mike Slinkman <mike.slinkman@gmail.com>
2020-09-28 18:51:28 +0200

    Changed the CountrySeederServiceProvider so that all seed / seeder files are placed in the same folder

--- src/CountrySeederServiceProvider.php
+++ src/CountrySeederServiceProvider.php
@@ -28,7 +28,7 @@ class CountrySeederServiceProvider extends ServiceProvider
             __DIR__ . '/../database/migrations/create_countries_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_countries_table.php'),
             __DIR__ . '/../database/seeds/BaseSeeder.php' => database_path('seeders/BaseSeeder.php'),
             __DIR__ . '/../database/seeds/CountryTableSeeder.php' => database_path('seeds/CountryTableSeeder.php'),
-            __DIR__ . '/../database/seeds/NationalityTableSeeder.php' => database_path('seeders/NationalityTableSeeder.php'),
+            __DIR__ . '/../database/seeds/NationalityTableSeeder.php' => database_path('seeds/NationalityTableSeeder.php'),
             __DIR__ . '/../database/migrations/create_nationalities_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_nationalities_table.php'),
         ], 'country-seeder');
 
